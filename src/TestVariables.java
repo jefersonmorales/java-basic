1f4cb99055a01ab6ce9c7e8cb532064ff48014fa
jefersonmorales-l <jefersmorales@gmail.com>
2023-06-07 15:21:37 -0600

    type numbers, variables, convert java

---
+++ src/TestVariables.java
@@ -0,0 +1,23 @@
+public class TestVariables {
+  public static void main(String[] args) {
+    
+    double salario = 1250;
+    System.out.println(salario);
+
+    double edad = 28;
+    System.out.println(edad);
+
+    double mitadSalario = salario / 2;
+    System.out.println(mitadSalario);
+  
+    int entero = 10;
+    double decimal = 4.0;
+    System.out.println(entero * decimal);
+
+
+    int entero1 = 4;
+    int entero2 = 2;
+    System.out.println(entero1 / entero2);
+    
+  }
+}
