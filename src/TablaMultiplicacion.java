6d589f6150b94dd2f565ec5d40f197d6921a0e5d
jefersonmorales-l <jefersmorales@gmail.com>
2023-06-11 22:55:55 -0600

    Ciclos o bucles anidados

---
+++ src/TablaMultiplicacion.java
@@ -0,0 +1,15 @@
+public class TablaMultiplicacion {
+  public static void main(String[] args) {
+    
+    for(int contador = 1;contador <=7;contador++) {
+      for (int multiplicacion = 1; multiplicacion <= 3;multiplicacion++) {
+        System.out.print(multiplicacion * contador);
+        System.out.print(" ");
+      }
+
+      System.out.println();
+      
+    }
+    
+  }
+}
