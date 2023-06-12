16a4eb86468a3726e6d9226f15b1dde3092f8d19
jefersonmorales-l <jefersmorales@gmail.com>
2023-06-11 22:37:28 -0600

    ciclo for

---
+++ src/CicloFor.java
@@ -0,0 +1,11 @@
+public class CicloFor {
+  public static void main(String[] args) {
+
+    for (int contador = 0; contador <= 10; contador++) {
+      System.out.println(contador);
+    }
+
+    // Para el caso de For no podremos llamar la variable de contador porque esta dentro del for
+    
+  }
+}
